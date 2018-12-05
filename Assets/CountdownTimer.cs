1ecf5ca162a41975c06895a3ab9353741ac932a6
Jiyoung Kim <1010215@student.sae.edu.au>
2018-12-05 21:49:14 +1000

    Canvas: Level, Timer, ClientState

---
+++ Assets/CountdownTimer.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    SphereProperties sphereManager;
+    public float currentTime;
+    float startingTime = 30f;
+    Text countdownText;
+
+    public int level;
+    public Text levelText;
+
+    public int clientState = 4;
+    public Text clientStateText;
+
+    bool loadFinished = false;
+    bool allMatch = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sphereManager = GameObject.Find("SphereManager").GetComponent<SphereProperties>();
+        countdownText = GetComponent<Text>();
+        level = 1;
+        resetTimer();
+        levelText.text = level.ToString("0");
+    }
+
+    void resetTimer()
+    {
+        currentTime = startingTime;
+        countdownText.gameObject.GetComponent<Animator>().SetBool("IsBelow10", false);
+        countdownText.gameObject.GetComponent<Animator>().SetBool("IsBelow5", false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+                
+        if (currentTime < 11)
+        {
+            countdownText.gameObject.GetComponent<Animator>().SetBool("IsBelow10", true);
+        }
+        if (currentTime < 5)
+        {
+            countdownText.gameObject.GetComponent<Animator>().SetBool("IsBelow5", true);
+        }
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            if (!loadFinished)
+            {
+                StartCoroutine(TimeOver());
+            }
+
+        }
+
+        foreach (bool matching in sphereManager.matchProp)
+        {
+            if (matching)
+            {
+                allMatch = true;
+            } else
+            {
+                allMatch = false;
+                break;
+            }
+
+            if (allMatch)
+            {
+                if (!loadFinished)
+                {
+                    StartCoroutine(Success());
+                }
+            }
+        }
+    }
+
+
+    IEnumerator TimeOver()
+    {
+        loadFinished = true;
+        Debug.Log("Time over!");
+        clientState--;
+        if(clientState == 3)
+        {
+            //Annoyed
+            clientStateText.text = "Annoyed";
+        }
+        if (clientState == 2)
+        {
+            //Angry
+            clientStateText.text = "Angry";
+        }
+        if (clientState == 1)
+        {
+            //About to explode...
+            clientStateText.text = "...";
+        } if (clientState == 0)
+        {
+            Debug.Log("Game Over!");
+        }
+
+
+        yield return new WaitForSeconds(1.5f);
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        resetTimer();
+        loadFinished = false;
+        yield break;
+    }
+
+    IEnumerator Success()
+    {
+        loadFinished = true;
+        Debug.Log("Success!");
+        level++;
+        levelText.text = level.ToString("0");
+
+        startingTime--;
+
+        yield return new WaitForSeconds(1.5f);
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        resetTimer();
+        loadFinished = false;
+        yield break;
+
+    }
+
+}
---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+   
+    void Start()
+    {
+        var canvasObject = GameObject.FindGameObjectsWithTag("Canvas");
+        int numCanvas = canvasObject.Length;
+        if(numCanvas != 1)
+        {
+            Destroy(canvasObject[1].gameObject);
+        } else
+        {
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    
+}
